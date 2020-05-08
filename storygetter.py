190c6ebb7acd88baceac79b5b9f20a29f62c2245
therealhe1ko <he1ko@protonmail.com>
2020-05-08 10:31:09 +0200

    changed wrong bool operator

--- storygetter.py
+++ storygetter.py
@@ -49,16 +49,16 @@ def getcred():
 	insturl = 'https://instagram.com/stories/' + name + '/'
 	global username
 	global password
-	if Config.get('Default_Creds', 'username') != 'johndoe':
+	if Config.get('Default_Creds', 'username') == 'johndoe':
 		print('\nYour instagram username of email')
 		username = input('Username/Email: ')
 	else:
-    	username = Config.get('Default_Creds', 'username')
-	if Config.get('Default_Creds', 'password') != 'hunter':
+		username = Config.get('Default_Creds', 'username')
+	if Config.get('Default_Creds', 'password') == 'hunter':
 		print('\nYour instagram password')
 		password = getpass('Password: ')
 	else:
-    	password = Config.get('Default_Creds', 'password')
+		password = Config.get('Default_Creds', 'password')
 
 
 def checkexist():
