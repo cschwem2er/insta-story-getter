4b47d46969f86c12e210bb8347e4cf1d013540cd
therealhe1ko <he1ko@protonmail.com>
2020-05-09 11:25:51 +0200

    feat: convert videos to images

---
+++ extractimages.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+import os
+
+def extractfrvid():
+    try:
+        os.popen('ffmpeg -version').read()
+    except:
+        print('FFMPEG needs to be installed!')
+
+    files = os.listdir('vid')
+
+    for file in files:
+        cmd = 'ffmpeg -ss 0.5 -i {} -vframes 1 -f image2 {}.jpg'.format('vid/{}'.format(file), 'img/%03d{}'.format(file[:-4]))
+        os.popen(cmd).read()
+
+if __name__ == "__main__":
+    extractfrvid()
